1d24c5550b22f426e1c8a0380ff1dca3c165b6ae
henthaii <200550625+henthaii@users.noreply.github.com>
2026-05-16 19:47:05 -0500

    started adding skeleton for js

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,30 @@
-// Gameboard is this an object?
+// Gameboard is this an object? This will hold the logic below
 
 function Gameboard() {
     const rows = 3;
     const columns = 3;
     const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
 }
+
+// Cell logic, this will show what should happen to the cell when you click on it and stuff
+
+function Cell() {
+
+}
+
+// GameController will be how the game works
+
+function GameController () {
+
+}
+
+const game = GameController();
